6d9f121cab134ca7f123f48fdd7e4f8290c084cf
Sergey Emelyanov <mrleondono@gmail.com>
2024-04-21 17:13:19 +0300

    added: URL validation, wiping input after submit, state for input

--- src/components/IssueFetcher/IssueFetcher.tsx
+++ src/components/IssueFetcher/IssueFetcher.tsx
@@ -1,24 +1,30 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import axios from 'axios';
+import cn from 'classnames';
 
 import styles from './IssueFetcher.module.scss';
 import { transformUrl } from '../../utils/transformUrl';
+import { isGitHubLink } from '../../utils/isGitHubLink';
 import { useAppDispatch } from '../../app/hooks';
 import { set as issuesSet } from '../../features/issues/issuesSlice';
-import { Issue } from '../../types/Issue';
 import { addIssue } from '../../features/columns/columnsSlice';
+import { Issue } from '../../types/Issue';
 
 export const IssueFetcher: FC = () => {
   const dispatch = useAppDispatch();
+  const [hasUrlError, setHasUrlError] = useState(false);
+  const [inputValue, setInputValue] = useState('');
 
   const handlerSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const repoUrl = (
-      event.currentTarget.elements.namedItem('repoUrl') as HTMLInputElement
-    ).value;
+    if (!isGitHubLink(inputValue)) {
+      setHasUrlError(true);
 
-    const apiUrl = transformUrl(repoUrl);
+      return;
+    }
+
+    const apiUrl = transformUrl(inputValue);
 
     try {
       const { data }: { data: Issue[] } = await axios.get(apiUrl);
@@ -26,21 +32,47 @@ export const IssueFetcher: FC = () => {
       dispatch(issuesSet(data));
 
       data.map((issue) => dispatch(addIssue(issue.id)));
+
+      setInputValue('');
+      setHasUrlError(false);
     } catch (error) {
       alert('Something went wrong during issues loading 😥');
     }
   };
 
   return (
-    <form onSubmit={handlerSubmit} className={styles.issueFetcher}>
-      <input
-        className={styles.issueFetcher__input}
-        name="repoUrl"
-        type="text"
-        placeholder="Enter repo URL"
-      />
+    <form
+      onBlur={() => setHasUrlError(!isGitHubLink(inputValue))}
+      onSubmit={handlerSubmit}
+      className={styles.issueFetcher}
+    >
+      <div className={styles.issueFetcher__inputWrapper}>
+        <span
+          className={cn(styles.issueFetcher__inputWrongMessage, {
+            [styles['issueFetcher__inputWrongMessage--show']]: hasUrlError,
+          })}
+        >
+          Invalid URL
+        </span>
 
-      <button type="submit" className={styles.issueFetcher__submitBtn}>
+        <input
+          className={cn(styles.issueFetcher__input, {
+            [styles['issueFetcher__input--wrong']]: hasUrlError,
+          })}
+          name="repoUrl"
+          type="text"
+          placeholder="Enter repo URL"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className={cn(styles.issueFetcher__submitBtn, {
+          [styles['issueFetcher__submitBtn--wrong']]: hasUrlError,
+        })}
+      >
         Load issues
       </button>
     </form>
