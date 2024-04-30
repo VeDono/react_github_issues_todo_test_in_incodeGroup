71171e505ec98c170bc75868436d1831ee39de97
VeDono <mrleondono@gmail.com>
2024-04-30 17:26:46 +0300

    added test for breadcrumbs, and rules for prettier

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
+/* eslint-disable prettier/prettier */
 /// <reference types="cypress" />
-
 
 describe('Todo issues E2E', () => {
   beforeEach(() => {
@@ -17,12 +17,39 @@ describe('Todo issues E2E', () => {
 
     cy.get('input[type="text"]').should('have.value', '');
 
-    cy.get('input[type="text"]').type('https://github.com/VeDono/mavic').should('have.value', 'https://github.com/VeDono/mavic');
+    cy.get('input[type="text"]')
+      .type('https://github.com/VeDono/mavic')
+      .should('have.value', 'https://github.com/VeDono/mavic');
+
     cy.contains('Load issues').click();
 
     cy.wait(['@getIssues', '@getRepo']);
 
-    cy.get('article[data-rbd-droppable-id="todo"]').find('div.ant-card').should('exist');
+    cy.get('article[data-rbd-droppable-id="todo"]')
+      .find('div.ant-card')
+      .first()
+      .should('contain', 'That is second issue example');
+
     cy.get('input[type="text"]').should('have.value', '');
   });
-})
+
+  it('should have breadcrumbs', () => {
+    cy.intercept('GET', '**/issues').as('getIssues');
+    cy.intercept('GET', '**/repos/VeDono/mavic').as('getRepo');
+
+    cy.get('input[type="text"]')
+      .type('https://github.com/VeDono/mavic');
+
+    cy.contains('Load issues').click();
+
+    cy.wait(['@getIssues', '@getRepo']);
+
+    cy.get('a')
+      .first()
+      .should('contain', 'VeDono');
+
+    cy.get('a')
+      .eq(1)
+      .should('contain', 'mavic');
+  });
+});
